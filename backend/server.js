a7149eaee48f3af9c9d43789c18ecf13a449d405
Riadh mouamnia <riadh.mouamnia@gmail.com>
2023-12-27 16:44:56 +0100

    feat: add user model, controller, and route to the backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 const mongoose = require("mongoose");
 
 // express app
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
 
 /// routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 // connect to db
 mongoose
