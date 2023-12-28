6639b65f5d128d13620262a448e573f2190504a5
Riadh mouamnia <riadh.mouamnia@gmail.com>
2023-12-28 13:55:30 +0100

    feat: add validation to signup method

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,17 @@ const userSchema = new Schema({
 // static signup method
 userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email });
+
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
 
   if (exists) {
     throw Error("Email already in use");
